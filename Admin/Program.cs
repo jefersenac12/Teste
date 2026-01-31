6a9775676890b81e67ad6269d0932345a2b1968d
jefersenac12 <jeferson.souza10@senacsp.edu.br>
2026-01-30 21:33:25 -0300

    Funcionalidade: configurar a URL base da API e registrar o ApiService
    Adicione a seção ApiSettings com a configuração BaseUrl ao arquivo appsettings.json.
    
    Registre o ApiService com injeção de dependência HttpClient no arquivo Program.cs.

--- Admin/Program.cs
+++ Admin/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<Admin.Services.ApiService>();
 
 var app = builder.Build();
 
